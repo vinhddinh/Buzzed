27df14d548e9e73a7f057989362e4882ae071c42
Eddie <edwardochapman@gmail.com>
2022-05-22 23:45:14 +1000

    Button presses and unfinished net code

--- buzzed/controllers/GamePlayerViewController.swift
+++ buzzed/controllers/GamePlayerViewController.swift
@@ -6,18 +6,89 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
 class GamePlayerViewController: UIViewController {
+    var peerID = MCPeerID (displayName: UIDevice.current.name)
+    var mcSession: MCSession!
+    var browser: MCBrowserViewController!
+    var advertiser: MCAdvertiserAssistant?
+    var delegate: MPCHandlerDelegate?
 
+    var buzzed: Bool = false
+    
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var buttonImage: UIImageView!
     @IBAction func buttonDown(_ sender: Any) {
-        buttonImage.image = UIImage(named: "buttonBuzzed.png")
+        if !buzzed {
+            lockBuzzer()
+            
+            // Make call to the host that this peer buzzed in
+            if mcSession.connectedPeers.count > 0 {
+                do {
+                    // Broadcast peer ID for host to receive
+                    try mcSession.send(Data(peerID.displayName.utf8), toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch let error as NSError {
+                    // Handle error
+                    print(error.localizedDescription);
+                }
+            }
+        }
     }
-    //    override func viewDidLoad() {
-//        super.viewDidLoad()
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        if let serverMessage = String(data: data, encoding: .utf8) {
+            DispatchQueue.main.async { [unowned self] in
+                if (serverMessage == "LOCKBUZZERS")
+                {
+                    lockBuzzer()
+                }
+                else if (serverMessage == "RESETBUZZZERS")
+                {
+                    resetBuzzer()
+                }
+            }
+        }
+    }
+
+    
+//    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+//        if let playerName = String(data: data, encoding: .utf8) {
+//            DispatchQueue.main.async { [unowned self] in
+//                if (playerName != "LOCKBUZZERS")
+//                {
+//                    // TODO: Identify player with name playerName as having buzzed in first
 //
-//        // Do any additional setup after loading the view.
+//                    // Tell all players to lock their buzzers
+//                    if mcSession.connectedPeers.count > 0 {
+//                        do {
+//                            // Broadcast message for players to lock buzzers
+//                            try mcSession.send(Data("LOCKBUZZERS".utf8), toPeers: mcSession.connectedPeers, with: .reliable)
+//                        } catch let error as NSError {
+//                            // Handle error
+//                            print(error.localizedDescription);
+//                        }
+//                    }
+//                }
+//            }
+//        }
 //    }
+    
+    func lockBuzzer() {
+        buttonImage.image = UIImage(named: "buttonBuzzed.png")
+        buzzed = true
+    }
+    
+    func resetBuzzer() {
+        buttonImage.image = UIImage(named: "buttonBuzz.png")
+        buzzed = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        button.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
     
 
     /*
