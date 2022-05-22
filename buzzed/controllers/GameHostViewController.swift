7a08636ce8824845e1ab0a38bc33d342b29bc223
elewho <32118433+elewho@users.noreply.github.com>
2022-05-22 18:31:58 +1000

    oh lawd

--- buzzed/controllers/GameHostViewController.swift
+++ buzzed/controllers/GameHostViewController.swift
@@ -6,18 +6,95 @@
 //
 
 import UIKit
+import CoreData
 
-class GameHostViewController: UIViewController {
+class GameHostViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var confirmEndView: UIView!
     @IBOutlet var blurView: UIVisualEffectView!
+    @IBOutlet weak var tableView: UITableView!
+//    var playerIndex: Int = 0
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var players:[PlayerMO]? //player managed object
+
+    //Height of each cell
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        //must change to number of players connected
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
+        let player = self.players?[indexPath.row]
+        
+        
+        cell.deviceNameLabel?.text = "iPhone 11"
+        cell.pointsLabel?.text = String(0)
+        cell.addButton.tag = indexPath.row //TODO: point system adding
+        cell.addButton.addTarget(self, action: #selector(rowWasTapped(sender:)), for: .touchUpInside)
+//        figures out what row the button was tapped in
+        return cell
+    }
+
+    @objc
+    func rowWasTapped(sender: UIButton){
+        let rowIndex: Int = sender.tag
+        let playerSelected = self.players?[rowIndex]
+        playerSelected?.pointsScored += 1
+    }
+    
+    /***************
+     Core Data Functions
+     ************************/
+    func fetchPlayers(){
+        do{
+            let request = PlayerMO.fetchRequest() as NSFetchRequest<PlayerMO>
+            let sortByScore = NSSortDescriptor(key: "pointsScored", ascending: false)
+            request.sortDescriptors = [sortByScore]
+
+            self.players = try context.fetch(request)
+            DispatchQueue.main.async {
+                if let tableview = self.tableView{
+                    tableview.reloadData()
+                }
+            }
+        } catch {
+            print("Error in fetching players")
+        }
+    }
+    
+    @IBAction func testPopulateButtonPressed(_ sender: Any) {
+        let newPlayer = PlayerMO(context: self.context)
+        newPlayer.deviceName = "Apple"
+        newPlayer.pointsScored = 0
+        
+        //save object
+        do{
+            try self.context.save()
+        } catch {
+            print("Error in saving player object")
+        }
+        
+        //refresh the tableview
+        self.fetchPlayers()
+    }
+    
+    /* UI Functions */
     //Spawns in confirmEndView
     @IBAction func endGameButtonPressed(_ sender: Any) {
         confirmEndView.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width * 0.9, height: self.view.bounds.height * 0.5) //width is 90% screen and height is 50% height
         blurView.bounds = self.view.bounds;
         animateIn(view: blurView);
         animateIn(view: confirmEndView);
+    }
+    
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+        mpcHandler.advertiser?.stop()
     }
     
     //Animates in blurred background
@@ -35,11 +112,13 @@ class GameHostViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mpcHandler.setupPeerWithDisplayName(displayName: UIDevice.current.name)
+        mpcHandler.setupSession()
+        mpcHandler.advertiseSelf(advertise: true)
     }
-    
 
+    
+    
     /*
     // MARK: - Navigation
 
--- buzzed/controllers/HostListViewController.swift
+++ buzzed/controllers/HostListViewController.swift
@@ -6,16 +6,63 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class HostListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class HostListViewController: UIViewController, MCSessionDelegate, MPCHandlerDelegate, MCBrowserViewControllerDelegate {
+    
+    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+    func changed(state: MCSessionState, of peer: MCPeerID) {
+        
+    }
+    
+    func received(data: Data, from peer: MCPeerID) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
     }
     
 
+    @IBOutlet weak var browserTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mpcHandler.setupPeerWithDisplayName(displayName: UIDevice.current.name)
+        mpcHandler.setupBrowser()
+        mpcHandler.browser.delegate = self
+        self.present(mpcHandler.browser, animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +72,5 @@ class HostListViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
